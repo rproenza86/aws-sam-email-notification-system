ba452ce9bbcf169c1bec97cb34e443d8fbca6dea
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2020-03-27 05:59:26 -0400

    chore: Remove build js files

--- src/processNotifications/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Status;
-(function (Status) {
-    Status["Saved"] = "Saved"; // FIXME: This is on TBD
-})(Status = exports.Status || (exports.Status = {}));
--- src/sendEmails/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = async (event = {}) => {
-    // Log the event argument for debugging and for use in local development.
-    const response = JSON.stringify(event, undefined, 2);
-    console.log(response);
-    return response;
-};
