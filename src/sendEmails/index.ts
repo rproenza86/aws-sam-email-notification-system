98f05c98bed63ec57ae12a7a84bc10ffd5157468
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2020-03-27 07:39:50 -0400

    feat: Save email data into db once sent email

--- src/sendEmails/index.ts
+++ src/sendEmails/index.ts
@@ -1,7 +1,28 @@
-export const handler = async (event: any = {}): Promise<any> => {
-  // Log the event argument for debugging and for use in local development.
-  const response = JSON.stringify(event, undefined, 2);
-  console.log(response);
+import { DynamoDB, AWSError } from 'aws-sdk';
+import * as uuid from 'uuid';
+// internal dependencies
+import { IEmailInfo } from '../processNotifications/types';
 
-  return response;
-}
+export const handler = async (emailInfo: IEmailInfo) => {
+    const dynamodb = new DynamoDB.DocumentClient();
+    const tableName = process.env.TABLE_NAME;
+
+    const params = {
+        TableName: tableName,
+        Item: {
+            id: uuid.v1(),
+            emailMessage: emailInfo?.content?.message,
+            sendTo: emailInfo?.content?.message_id
+        },
+        ConditionExpression: 'attribute_not_exists(id)',
+        ReturnConsumedCapacity: 'TOTAL'
+    };
+    console.log(`Processing email for :`, emailInfo);
+
+    const dbSavingResult = await dynamodb.put(params).promise();
+    const result = { dbSavingResult, emailInfo };
+
+    console.log('Item added to table: ' + tableName);
+
+    return result;
+};
