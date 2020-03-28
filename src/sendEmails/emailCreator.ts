daef1bba849b7d553a18c844e0e975b1ba27d35e
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2020-03-28 15:19:33 -0400

    feat: Add tests email boxes

--- src/sendEmails/emailCreator.ts
+++ src/sendEmails/emailCreator.ts
@@ -5,7 +5,14 @@ export const createEmailParams = (message: string) => {
 
     // Replace recipient@example.com with a "To" address. If your account
     // is still in the sandbox, this address must be verified.
+    // valid email
     const recipient = 'rproenza86@gmail.com';
+
+    // use to test bounce
+    // const recipient = 'bounce@simulator.amazonses.com';
+
+    // use to test complaint
+    // const recipient = 'complaint@simulator.amazonses.com';
 
     // Specify a configuration set. If you do not want to use a configuration
     // set, comment the following variable, and the
