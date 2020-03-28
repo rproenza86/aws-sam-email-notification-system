dac9d661b472992cadf5ea97990b512fed411e66
Raúl Rodríguez Proenza <raul.r.proenza@gmail.com>
2020-03-28 19:22:25 -0400

    feat: Update email table on email bounce event

--- src/emailBounceService/index.ts
+++ src/emailBounceService/index.ts
@@ -1,7 +1,41 @@
-export const handler = async (event: any = {}): Promise<any> => {
-  // Log the event argument for debugging and for use in local development.
-  const response = JSON.stringify(event, undefined, 2);
-  console.log(response);
+// external dependencies
+import { DynamoDB } from 'aws-sdk';
 
-  return response;
-}
+export const handler = async (event: AWSLambda.SNSEvent, context: AWSLambda.Context) => {
+    const updates = [];
+    let result: any = { success: true, updates, event };
+
+    for (const bounceEvent of event.Records) {
+        const messageId = (bounceEvent.Sns.Message as any)?.mail?.messageId;
+
+        if (messageId) {
+            const dynamodb = new DynamoDB.DocumentClient();
+            const tableName = process.env.TABLE_NAME;
+
+            const params = {
+                TableName: tableName,
+                Key: {
+                    id: messageId
+                },
+                UpdateExpression: 'set sentTraceData=:r',
+                ExpressionAttributeValues: {
+                    ':r': bounceEvent.Sns.Message
+                },
+                ReturnValues: 'UPDATED_NEW'
+            };
+            console.log(`Processing email bounced!`);
+
+            const dbUpdateResult = await dynamodb.update(params).promise();
+
+            console.log('Item updated with email bounce data on table: ' + tableName);
+
+            updates.push({
+                dbUpdateResult,
+                bounceEvent: bounceEvent.Sns.Message
+            });
+        }
+    }
+
+    console.log(result);
+    return result;
+};
